ef68b09ee416c855bb0a12cd34e1d5f3721df439
Chivan <huainian361@hotmail.com>
2019-05-12 23:55:40 +0800

    添加订单评价

--- lib/redux/login/login_middle.dart
+++ lib/redux/login/login_middle.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hair/common/global_navigator.dart';
 import 'package:hair/common/regist_route.dart';
 import 'package:hair/config/server_api.dart';
@@ -50,15 +48,16 @@ class LoginMiddleware extends MiddlewareClass<AppState> {
         store.dispatch(new LoginFailedAction());
         toastMsg = '登录失败，请重试';
       }
-      Fluttertoast.showToast(
-        msg: toastMsg,
-        toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIos: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0,
-      );
+      print("$toastMsg");
+//      Fluttertoast.showToast(
+//        msg: toastMsg,
+//        toastLength: Toast.LENGTH_LONG,
+//        gravity: ToastGravity.CENTER,
+//        timeInSecForIos: 1,
+//        backgroundColor: Colors.red,
+//        textColor: Colors.white,
+//        fontSize: 16.0,
+//      );
     }
 
     //  注册
