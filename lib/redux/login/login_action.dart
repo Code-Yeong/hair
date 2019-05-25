ae0df05e69b07b28950745776a38f07a1bd77dea
Chivan <huainian361@hotmail.com>
2019-05-25 22:26:32 +0800

    注视

--- lib/redux/login/login_action.dart
+++ lib/redux/login/login_action.dart
@@ -1,10 +1,12 @@
 import 'package:hair/utils/enum.dart';
 
+//改变角色
 class ChangeRoleAction {
   final Role role;
   ChangeRoleAction({this.role});
 }
 
+//开始登录
 class BeginLoginAction {
   final String phone;
   final String name;
@@ -13,12 +15,16 @@ class BeginLoginAction {
   BeginLoginAction({this.phone, this.name, this.password});
 }
 
+//登录成功
 class LoginSuccessAction {}
 
+//登录失败
 class LoginFailedAction {}
 
+//注销
 class LoginOutAction {}
 
+//开始注册
 class BeginSignupAction {
   final String phone;
   final String name;
@@ -27,8 +33,10 @@ class BeginSignupAction {
   BeginSignupAction({this.phone, this.name, this.password});
 }
 
+//注册成功
 class SignupSuccessAction {}
 
+//注册失败
 class SignupFailedAction {}
 //
 //class LoginOutAction {}
